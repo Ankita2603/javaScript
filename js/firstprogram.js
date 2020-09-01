b48ad826887a896e74a7b3c823ab44709390c124
Shashi Singh <shashi.we@gmail.com>
2020-09-01 19:01:43 +0530

    First commit

---
+++ js/firstprogram.js
@@ -0,0 +1,23 @@
+// var name = 'Ankita';
+// console.log(name);
+// surname = "Nandanwar";
+// console.log(surname);
+// console.log(name+" "+surname);
+
+// var greeting;
+// console.log(greeting);
+
+// greeting="Hey Ankita";
+// console.log(greeting);
+
+
+  // letters, numbers, _ , $ (can't start with number)
+
+// var 1name ="Ankita";
+// console.log(1name);   //error
+
+// var $name ='Ankita';
+// console.log($name);
+
+var _name ='Ankita';
+console.log(_name);
